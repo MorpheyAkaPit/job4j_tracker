b31668fed6872dc53529b3646f79bd880403ade8
Pit <pit777@rambler.ru>
2023-09-28 22:54:41 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Wolf.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Wolf {
+
+    public void eat(Girl girl) {
+
+    }
+
+    public void tryEat(Ball ball) {
+        System.out.print("Волк - ");
+        ball.tryRun(false);
+    }
+}
