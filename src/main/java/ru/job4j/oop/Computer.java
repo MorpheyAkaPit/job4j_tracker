ba08b9a3fa44e03ba3cf197a80338fd3fc1b7238
Pit <pit777@rambler.ru>
2023-10-19 22:20:20 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -10,6 +10,16 @@ public class Computer {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: "+ ssd + " Gb");
         System.out.println("Модель CPU: " + cpu);
+        System.out.println();
+    }
+
+    public  Computer() {
+
+    }
+
+    public Computer(int ssd, String cpu) {
+            this.ssd = ssd;
+            this.cpu = cpu;
     }
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
@@ -17,10 +27,21 @@ public class Computer {
         this.cpu = cpu;
     }
 
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+            this.multiMonitor = multiMonitor;
+            this.ssd = (int) ssd;
+            this.cpu = cpu;
+    }
+
     public static void main(String[] args) {
-        Computer computer = new Computer(true, 500, "Intel Core i7-10700K");
-        computer.printInfo();
-        Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
-        comp.printInfo();
+        Computer first = new Computer();
+        first.printInfo();
+        Computer second = new Computer(true, 256, "AMD Ryzen 5 3600");
+        second.printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        forth.printInfo();
+
     }
 }
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -19,4 +19,31 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Item() {
+
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public void printInfo() {
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Item first = new Item();
+        first.printInfo();
+        Item second = new Item(25);
+        second.printInfo();
+        Item third = new Item(25, "Jack");
+        third.printInfo();
+
+    }
 }
